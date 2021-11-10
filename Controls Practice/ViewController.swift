f51d0de6a785af804930371878f89d8e38c6d965
Aficionado <volodiag@mail.ru>
2021-11-10 14:35:05 +0200

    Implemented switches setting and getting

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var button: UIButton!
     @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
@@ -22,25 +22,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotateSwitches()
         updateUI()
+    }
+    
+    /// Rotates switches to be vertically oriented
+    func rotateSwitches() {
+        for `switch` in switches {
+            `switch`.layer.transform = CATransform3DMakeRotation(-.pi / 2, 0, 0, 1)
+        }
+    }
+    
+    /// Updates number from the switches set
+    func updateNumberFromSwitches() {
+        var number = 0
+        for `switch` in switches {
+            number += `switch`.isOn ? `switch`.tag : 0
+        }
+        self.number = UInt8(number % 256)
+    }
+    
+    /// Udpates switches from the number
+    func updateSwitches() {
+//        var number = self.number
+        for `switch` in switches.reversed() {
+//            if UInt8(`switch`.tag) < number {
+//                `switch`.isOn = true
+//                number -= UInt8(`switch`.tag)
+//            } else if UInt8(`switch`.tag) > number {
+//                `switch`.isOn = false
+//            } else {
+//                `switch`.isOn = true
+//                number -= UInt8(`switch`.tag)
+//            }
+            // Below is more simple way of updating switches
+            `switch`.isOn = Int(number) & `switch`.tag != 0
+        }
     }
     
     /// Updates all outlets number
     func updateUI() {
+        
         button.setTitle("\(number)", for: [])
         
-        // TODO: Set switches to number
+        updateSwitches()
         
         slider.value = Float(number)
+        
         textField.text = "\(number)"
     }
-
+    
     @IBAction func buttonPressed() {
         number = UInt8((Int(number) + 1) % 256)
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
-        print(#line, #function, sender.tag)
+        updateNumberFromSwitches()
     }
     
     @IBAction func sliderMoved() {
